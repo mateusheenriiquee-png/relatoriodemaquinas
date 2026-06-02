cdda25387a057cea2e4b0a573d2b5f6d48ef6589
mateusheenriiquee-png <mateus.heenriiquee@gmail.com>
2026-06-02 10:49:49 -0300

    dkfhjjdkshf

--- public/js/config/firebase.js
+++ public/js/config/firebase.js
@@ -3,14 +3,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMpPJPrhEcDUOgQEg5vVpHMEKThGu31y4",
-  authDomain: "suportetecnico-api.firebaseapp.com",
-  projectId: "suportetecnico-api",
-  storageBucket: "suportetecnico-api.firebasestorage.app",
-  messagingSenderId: "192795919231",
-  appId: "1:192795919231:web:7385d3a7e6ba418a2e504a"
+  apiKey: "AIzaSyBzCwpa4Z_jUh1kIw5tdGXF69nG4Hdi4Ic",
+  authDomain: "suportetecnico-api-9386b.firebaseapp.com",
+  projectId: "suportetecnico-api-9386b",
+  storageBucket: "suportetecnico-api-9386b.firebasestorage.app",
+  messagingSenderId: "790334341463",
+  appId: "1:790334341463:web:1d726fde261c955d6a3e12"
 };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
